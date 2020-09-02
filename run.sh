3b5a7d3be7a3ede98fea7e13a63272c2efc077fa
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-09-02 13:48:32 -0700

    Add some openssl verify commands. (#6)

--- run.sh
+++ run.sh
@@ -14,6 +14,10 @@ ceremony --config r4-key.yaml
 ceremony --config r3-cross-csr.yaml
 ceremony --config r4-cross-csr.yaml
 
+# Verify the self-signature on these CSRs.
+openssl req -verify -in int-r3.cross-csr.pem -noout
+openssl req -verify -in int-r4.cross-csr.pem -noout
+
 # Simulating root HSM
 ceremony --config root-x1.yaml
 ceremony --config root-x2.yaml
@@ -24,6 +28,25 @@ ceremony --config r4-cert.yaml
 ceremony --config root-x1.crl.yaml
 ceremony --config root-x2.crl.yaml
 ceremony --config x2-signed-by-x1.yaml
+
+# Verify the root -> intermediate signatures, plus the TLS Server Auth EKU.
+# -check_ss_sig means to verify the root certificate's self-signature.
+# 1609459200 is January 1 2021; this is necessary because we're testing with NotBefore in the future.
+openssl verify -check_ss_sig -attime 1609459200 -CAfile root-x2.cert.pem -purpose sslserver int-e1.cert.pem int-e2.cert.pem
+openssl verify -check_ss_sig -attime 1609459200 -CAfile root-x1.cert.pem -purpose sslserver int-r3.cert.pem int-r3.cert.pem
+
+# Verify the X1 -> X2 cross-signature.
+# Don't verify `-purpose sslserver` here because x2-signed-by-x1 intentionally
+# doesn't have the "TLS Server Auth" EKU (and doesn't need it).
+openssl verify -check_ss_sig -attime 1609459200 -CAfile root-x1.cert.pem x2-signed-by-x1.cert.pem
+
+# Verify the path from X1 -> X2 -> E1 and X1 -> X2 -> E2, plus the TLS Server Auth EKU.
+openssl verify -check_ss_sig -attime 1609459200 -CAfile root-x1.cert.pem -purpose sslserver -untrusted x2-signed-by-x1.cert.pem int-e1.cert.pem
+openssl verify -check_ss_sig -attime 1609459200 -CAfile root-x1.cert.pem -purpose sslserver -untrusted x2-signed-by-x1.cert.pem int-e2.cert.pem
+
+# Verify the CRLs.
+openssl crl -verify -CAfile root-x1.cert.pem -in root-x1.crl.pem -noout
+openssl crl -verify -CAfile root-x2.cert.pem -in root-x2.crl.pem -noout
 
 rm root-x1.key.pem
 rm root-x1.cert.pem
