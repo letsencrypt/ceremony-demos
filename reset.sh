35edf9733be68482681634d7b0657031757e79a3
Phil Porada <philporada@gmail.com>
2023-05-31 16:58:27 -0400

    Allow cleaning up files through a symlink

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -exv
 
-find -type f \( -name '*.pem' -o -name '*.txt' \) -delete
+find -L -type f \( -name '*.pem' -o -name '*.txt' \) -delete
 rm -rf softhsm/*
 git reset -- softhsm
 git checkout -- softhsm
