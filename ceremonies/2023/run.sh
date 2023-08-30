af990d98c6d5286bf2ab999a7a32022beb6648ce
Phil Porada <philporada@gmail.com>
2023-08-30 14:01:18 -0400

    Update 2023 run.sh

--- ceremonies/2023/run.sh
+++ ceremonies/2023/run.sh
@@ -19,16 +19,11 @@ function _echo() {
 }
 
 function setup_ceremony_tool() {
-    TMPDIR="/tmp/ceremony-tools"
-    
-    if [ -z "${_CEREMONY_BIN_2023}" ]; then
-        export _CEREMONY_BIN_2023="${TMPDIR}/bin/2023/ceremony"
-    else
-        if [ -x "${_CEREMONY_BIN_2023}" ]; then
-            return 0
-        fi
+    if [ -n "${CEREMONY_BIN_2023}" ] && [ -x "${CEREMONY_BIN_2023}" ]; then
+        return 0
     fi
-    
+
+    TMPDIR="/tmp/ceremony-tools"
     mkdir -p "${TMPDIR}/bin/2023/"
     if [ ! -d "${TMPDIR}/boulder" ]; then
         git clone https://github.com/letsencrypt/boulder/ "${TMPDIR}/boulder"
@@ -48,52 +43,51 @@ function setup_ceremony_tool() {
 }
 
 setup_ceremony_tool
-if [ $? -ne 0 ]; then
-    exit 1
-fi
 
-_CEREMONY_BIN="${_CEREMONY_BIN_2023}"
+CEREMONY_BIN="${CEREMONY_BIN_2023}"
 CEREMONY_YEAR="$(basename "$(dirname "$(readlink -f "${0}")")")"
-_echo "Running ${CEREMONY_YEAR} ceremony with tooling at ${_CEREMONY_BIN}"
+_echo "Running ${CEREMONY_YEAR} ceremony with tooling at ${CEREMONY_BIN}"
 
 CEREMONY_DIR="$(dirname ${BASH_SOURCE[0]})"
 cd "${CEREMONY_DIR}"
 
-"${_CEREMONY_BIN}" --config "./e5-key.yaml"
-"${_CEREMONY_BIN}" --config "./e6-key.yaml"
-"${_CEREMONY_BIN}" --config "./e7-key.yaml"
-"${_CEREMONY_BIN}" --config "./e8-key.yaml"
-"${_CEREMONY_BIN}" --config "./e9-key.yaml"
-"${_CEREMONY_BIN}" --config "./i1-key.yaml"
-"${_CEREMONY_BIN}" --config "./r10-key.yaml"
-"${_CEREMONY_BIN}" --config "./r11-key.yaml"
-"${_CEREMONY_BIN}" --config "./r12-key.yaml"
-"${_CEREMONY_BIN}" --config "./r13-key.yaml"
-"${_CEREMONY_BIN}" --config "./r14-key.yaml"
+"${CEREMONY_BIN}" --config "./e5-key.yaml"
+"${CEREMONY_BIN}" --config "./e6-key.yaml"
+"${CEREMONY_BIN}" --config "./e7-key.yaml"
+"${CEREMONY_BIN}" --config "./e8-key.yaml"
+"${CEREMONY_BIN}" --config "./e9-key.yaml"
+"${CEREMONY_BIN}" --config "./i1-key.yaml"
+"${CEREMONY_BIN}" --config "./r10-key.yaml"
+"${CEREMONY_BIN}" --config "./r11-key.yaml"
+"${CEREMONY_BIN}" --config "./r12-key.yaml"
+"${CEREMONY_BIN}" --config "./r13-key.yaml"
+"${CEREMONY_BIN}" --config "./r14-key.yaml"
 
-"${_CEREMONY_BIN}" --config "./e5-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e6-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e7-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e8-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e9-cert.yaml"
-"${_CEREMONY_BIN}" --config "./i1-cert.yaml"
-"${_CEREMONY_BIN}" --config "./r10-cert.yaml"
-"${_CEREMONY_BIN}" --config "./r11-cert.yaml"
-"${_CEREMONY_BIN}" --config "./r12-cert.yaml"
-"${_CEREMONY_BIN}" --config "./r13-cert.yaml"
-"${_CEREMONY_BIN}" --config "./r14-cert.yaml"
+"${CEREMONY_BIN}" --config "./e5-cert.yaml"
+"${CEREMONY_BIN}" --config "./e6-cert.yaml"
+"${CEREMONY_BIN}" --config "./e7-cert.yaml"
+"${CEREMONY_BIN}" --config "./e8-cert.yaml"
+"${CEREMONY_BIN}" --config "./e9-cert.yaml"
+"${CEREMONY_BIN}" --config "./i1-cert.yaml"
+"${CEREMONY_BIN}" --config "./r10-cert.yaml"
+"${CEREMONY_BIN}" --config "./r11-cert.yaml"
+"${CEREMONY_BIN}" --config "./r12-cert.yaml"
+"${CEREMONY_BIN}" --config "./r13-cert.yaml"
+"${CEREMONY_BIN}" --config "./r14-cert.yaml"
 
-"${_CEREMONY_BIN}" --config "./e5-cross-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e6-cross-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e7-cross-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e8-cross-cert.yaml"
-"${_CEREMONY_BIN}" --config "./e9-cross-cert.yaml"
+"${CEREMONY_BIN}" --config "./e5-cross-cert.yaml"
+"${CEREMONY_BIN}" --config "./e6-cross-cert.yaml"
+"${CEREMONY_BIN}" --config "./e7-cross-cert.yaml"
+"${CEREMONY_BIN}" --config "./e8-cross-cert.yaml"
+"${CEREMONY_BIN}" --config "./e9-cross-cert.yaml"
 
-# Verify the root -> intermediate signatures, plus the TLS Server Auth EKU.
-# -check_ss_sig means to verify the root certificate's self-signature.
 
-## 1704067201 is Dec 31 2024; this is necessary because we're testing with NotBefore in the future.
-openssl verify -check_ss_sig -attime 1704067201 -CAfile "../2015/root-x1.cert.pem" -purpose sslserver \
+## 1704067201 is Dec 31, 2024; this ensures the check will still pass even after root-x1 and root-x2 expire.
+openssl verify \
+    -check_ss_sig \
+    -attime 1704067201 \
+    -CAfile "../2015/root-x1.cert.pem" \
+    -purpose sslserver \
     "./int-e5-cross.cert.pem" \
     "./int-e6-cross.cert.pem" \
     "./int-e7-cross.cert.pem" \
@@ -105,19 +99,31 @@ openssl verify -check_ss_sig -attime 1704067201 -CAfile "../2015/root-x1.cert.pe
     "./int-r13.cert.pem" \
     "./int-r14.cert.pem"
 
-openssl verify -check_ss_sig -attime 1704067201 -CAfile "../2020/root-x2.cert.pem" -purpose sslserver \
+openssl verify \
+    -check_ss_sig \
+    -attime 1704067201 \
+    -CAfile "../2020/root-x2.cert.pem" \
+    -purpose sslserver \
     "./int-e5.cert.pem" \
     "./int-e6.cert.pem" \
     "./int-e7.cert.pem" \
     "./int-e8.cert.pem" \
     "./int-e9.cert.pem"
 
-## 1695168000 is Sept 26, 2023
-openssl verify -check_ss_sig -attime 1695686400 -CAfile "../2020/root-x2.cert.pem" -purpose sslserver \
+## 1695168000 is Aug 30, 2023; this ensures the check will still pass even after root-x2 expires.
+openssl verify \
+    -check_ss_sig \
+    -attime 1695686400 \
+    -CAfile "../2020/root-x2.cert.pem" \
+    -purpose sslserver \
     "./int-i1.cert.pem"
 
 # Intermediate I1 is to be revoked after issuance and never used. It's purpose is to
 # give us operational experience revoking an intermediate. In production we'll need to
 # update a CRL.
-"${_CEREMONY_BIN}" --config "./root-x2.crl.yaml"
-openssl crl -inform PEM -in "./root-x2.crl.pem" -noout -crlnumber | grep -q crlNumber=0x6F || echo "Did not find expected CRL version"
+"${CEREMONY_BIN}" --config "./root-x2.crl.yaml"
+openssl crl \
+    -inform PEM \
+    -in "./root-x2.crl.pem" \
+    -noout \
+    -crlnumber | grep -q crlNumber=0x6F || echo "Did not find expected CRL version"
