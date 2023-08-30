925baf045e1ce8aa189ff26d16400b7ae77bcb09
Phil Porada <philporada@gmail.com>
2023-08-30 13:56:30 -0400

    Address comments

--- ceremonies/2000/run.sh
+++ ceremonies/2000/run.sh
@@ -17,22 +17,18 @@ function _echo() {
 }
 
 function setup_ceremony_tool() {
-    TMPDIR="/tmp/ceremony-tools"
-    
-    if [ -z "${CEREMONY_BIN_HISTORIC}" ]; then
-        export CEREMONY_BIN_HISTORIC="${TMPDIR}/bin/PRE_2023/ceremony"
-    else
-        if [ -x "${CEREMONY_BIN_HISTORIC}" ]; then
-            return 0
-        fi
+    if [ -n "${CEREMONY_BIN_HISTORIC}" ] && [ -x "${CEREMONY_BIN_HISTORIC}" ]; then
+        return 0
     fi
-    
+
+    TMPDIR="/tmp/ceremony-tools"
+    export CEREMONY_BIN_HISTORIC="${TMPDIR}/bin/PRE_2023/ceremony"   
     mkdir -p "${TMPDIR}/bin/PRE_2023/"
     if [ ! -d "${TMPDIR}/boulder" ]; then
         git clone https://github.com/letsencrypt/boulder/ "${TMPDIR}/boulder"
     fi
 
-    if [ ! -x "${TMPDIR}/bin/PRE_2023/ceremony" ]; then
+    if [ ! -x "${CEREMONY_BIN_HISTORIC}" ]; then
         # Build ceremony on the commit prior to removing configuration of Policy OIDs.
         # This will allow all ceremonies prior to 2023 to complete successfully without
         # requiring backporting changes to those ceremonies and losing the historical
@@ -46,9 +42,6 @@ function setup_ceremony_tool() {
 }
 
 setup_ceremony_tool
-if [ $? -ne 0 ]; then
-    exit 1
-fi
 
 CEREMONY_YEAR="$(basename "$(dirname "$(readlink -f "${0}")")")"
 _echo "Running ${CEREMONY_YEAR} ceremony with tooling at ${CEREMONY_BIN_HISTORIC}"
--- ceremonies/2020/run.sh
+++ ceremonies/2020/run.sh
@@ -17,22 +17,18 @@ function _echo() {
 }
 
 function setup_ceremony_tool() {
-    TMPDIR="/tmp/ceremony-tools"
-    
-    if [ -z "${CEREMONY_BIN_HISTORIC}" ]; then
-        export CEREMONY_BIN_HISTORIC="${TMPDIR}/bin/PRE_2023/ceremony"
-    else
-        if [ -x "${CEREMONY_BIN_HISTORIC}" ]; then
-            return 0
-        fi
+    if [ -n "${CEREMONY_BIN_HISTORIC}" ] && [ -x "${CEREMONY_BIN_HISTORIC}" ]; then
+        return 0
     fi
-    
+
+    TMPDIR="/tmp/ceremony-tools"
+    export CEREMONY_BIN_HISTORIC="${TMPDIR}/bin/PRE_2023/ceremony"   
     mkdir -p "${TMPDIR}/bin/PRE_2023/"
     if [ ! -d "${TMPDIR}/boulder" ]; then
         git clone https://github.com/letsencrypt/boulder/ "${TMPDIR}/boulder"
     fi
 
-    if [ ! -x "${TMPDIR}/bin/PRE_2023/ceremony" ]; then
+    if [ ! -x "${CEREMONY_BIN_HISTORIC}" ]; then
         # Build ceremony on the commit prior to removing configuration of Policy OIDs.
         # This will allow all ceremonies prior to 2023 to complete successfully without
         # requiring backporting changes to those ceremonies and losing the historical
@@ -46,9 +42,6 @@ function setup_ceremony_tool() {
 }
 
 setup_ceremony_tool
-if [ $? -ne 0 ]; then
-    exit 1
-fi
 
 CEREMONY_YEAR="$(basename "$(dirname "$(readlink -f "${0}")")")"
 _echo "Running ${CEREMONY_YEAR} ceremony with tooling at ${CEREMONY_BIN_HISTORIC}"
@@ -74,14 +67,20 @@ cd "${CEREMONY_DIR}"
 "${CEREMONY_BIN_HISTORIC}" --config "./r3-cross-csr.yaml"
 "${CEREMONY_BIN_HISTORIC}" --config "./r4-cross-csr.yaml"
 
-# Verify the root -> intermediate signatures, plus the TLS Server Auth EKU.
-# -check_ss_sig means to verify the root certificate's self-signature.
-
-## 1609459200 is Dec 31 2021; this is necessary because we're testing with NotBefore in the future.
-openssl verify -check_ss_sig -attime 1609459200 -CAfile "../2015/root-x1.cert.pem" -purpose sslserver \
+## 1609459200 is Dec 31 2021; this ensures the check will still pass even after root-x1 expires.
+openssl verify \
+    -check_ss_sig \
+    -attime 1609459200 \
+    -CAfile "../2015/root-x1.cert.pem" \
+    -purpose sslserver \
     "./int-r3.cert.pem" \
     "./int-r4.cert.pem"
 
-openssl verify -check_ss_sig -attime 1609459200 -CAfile "./root-x2.cert.pem" -purpose sslserver \
+## 1609459200 is Dec 31 2021; this ensures the check will still pass even after root-x2 expires.
+openssl verify \
+    -check_ss_sig \
+    -attime 1609459200 \
+    -CAfile "./root-x2.cert.pem" \
+    -purpose sslserver \
     "./int-e1.cert.pem" \
     "./int-e2.cert.pem"
